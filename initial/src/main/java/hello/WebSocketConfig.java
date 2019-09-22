543dd317e47a53fe0b055b4e1e6184efdb2dc444
jonathan <pohjonathan@gmail.com>
2019-09-22 22:36:35 +0800

    First Commit

---
+++ initial/src/main/java/hello/WebSocketConfig.java
@@ -0,0 +1,29 @@
+/*
+Now that the essential components of the service are created,
+you can configure Spring to enable WebSocket and STOMP messaging.
+ */
+
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
